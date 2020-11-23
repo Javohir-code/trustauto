b659a7f6ff913a38cbd5900d07f855e636e505eb
Javohir Omonov <javokhiromonov77@gmail.com>
2020-11-23 18:33:00 +0500

    Fixing aws

--- routes/admins.js
+++ routes/admins.js
@@ -12,6 +12,6 @@ router.route('/add-admin').post(addAdmin);
 router.route('/login').post(loginAdmin);
 router.route('/seller').get(getSeller);
 router.route('/seller/details/:id').get(getSellerById);
-router.route('/seller/delete/:id').get(deleteSeller);
+router.route('/seller/delete/:id').delete(deleteSeller);
 
 module.exports = router;
